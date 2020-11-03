47c3d74223a30f26087540da5fb40ebe524d110c
boosk526 <boosk526@naver.com>
2020-11-03 15:38:34 +0900

    up

--- src/main/java/com/choa/s4/interceptor/QnaInterceptor.java
+++ src/main/java/com/choa/s4/interceptor/QnaInterceptor.java
@@ -2,16 +2,22 @@ package com.choa.s4.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.choa.s4.member.MemberDTO;
+
+@Component
 public class QnaInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		System.out.println("Controller 진입 전");
+		
 		return super.preHandle(request, response, handler);
 	}
 	
